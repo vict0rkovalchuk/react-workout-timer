9e71ed87fbeaaac2ef1cdf41ac4df9dd12385909
Victor Kovalchuk <vict02r05@gmail.com>
2026-04-13 14:39:01 +0300

    use useEffect for setting state based on other state updates

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import { memo, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import clickSound from './ClickSound.m4a';
 
 function Calculator({ workouts, allowSound }) {
@@ -7,15 +7,27 @@ function Calculator({ workouts, allowSound }) {
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  const [duration, setDuration] = useState(0);
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
+
+  useEffect(function () {
+    setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+  }, [number, sets, speed, durationBreak]);
 
   const playSound = function () {
     if (!allowSound) return;
     const sound = new Audio(clickSound);
     sound.play();
   };
+
+  function handleInc() {
+    setDuration(curDuration => Math.floor(curDuration) + 1);
+  }
+
+  function handleDec() {
+    setDuration(curDuration => curDuration > 1 ? Math.ceil(curDuration) - 1 : 0);
+  }
 
   return (
     <>
@@ -66,13 +78,13 @@ function Calculator({ workouts, allowSound }) {
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button onClick={handleDec}>–</button>
         <p>
           {mins < 10 && '0'}
           {mins}:{seconds < 10 && '0'}
           {seconds}
         </p>
-        <button onClick={() => {}}>+</button>
+        <button onClick={handleInc}>+</button>
       </section>
     </>
   );
